dea693250352afef9d439ff7e8ff1ed4c7e88fa8
Ahmed Al Mughirah <s202018480@kfupm.edu.sa>
2023-05-03 00:31:19 +0300

    w

--- App.java
+++ App.java
@@ -4,6 +4,7 @@ import java.util.List;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -13,11 +14,17 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -30,6 +37,8 @@ public class App extends Application {
     ArrayList<Tournament> tournaments = database.getTournaments();
     ArrayList<Student> students = database.getStudents();
     ArrayList<Team> teams = database.getTeams();
+    private final int WINDOW_WIDTH = 600;
+    private final int WINDOW_HEIGHT = 600;
 
     public static void main(String[] args) {
         launch(args);
@@ -39,32 +48,37 @@ public class App extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Tournament App");
 
+        primaryStage.setWidth(WINDOW_WIDTH);
+        primaryStage.setHeight(WINDOW_HEIGHT);
         // Scences
         Group loginGroup = new Group();
-        Scene loginScene = new Scene(loginGroup, 400, 275);
+        Scene loginScene = new Scene(loginGroup,600,600);
 
         Group mainGroup = new Group();
-        Scene mainScene = new Scene(mainGroup, 400, 275);
+        Scene mainScene = new Scene(mainGroup,600,600);
 
         Group tournamentGroup = new Group();
-        Scene tournamentScene = new Scene(tournamentGroup, 400, 275);
+        Scene tournamentScene = new Scene(tournamentGroup, 600, 600);
 
         Group addTournamentGroup = new Group();
-        Scene addTournamentScene = new Scene(addTournamentGroup, 400, 275);
+        Scene addTournamentScene = new Scene(addTournamentGroup, 600, 600);
 
         Group profileGroup = new Group();
-        Scene profileScene = new Scene(profileGroup, 400, 275);
+        Scene profileScene = new Scene(profileGroup, 600, 600);
 
         Group matchesGroup = new Group();
-        Scene matchesScene = new Scene(matchesGroup, 400, 275);
+        Scene matchesScene = new Scene(matchesGroup, 600, 600);
 
         Group addScoreGroup = new Group();
-        Scene addScoreScene = new Scene(addScoreGroup, 400, 275);
+        Scene addScoreScene = new Scene(addScoreGroup, 600, 600);
 
         Group selectedTournamnetGroup = new Group();
-        Scene selectedTournamnetScene = new Scene(selectedTournamnetGroup, 400, 275);
+        Scene selectedTournamnetScene = new Scene(selectedTournamnetGroup, 600, 600);
 
+        ///
         // Login page ( Login scene )
+        ///
+
         BorderPane borderPane = new BorderPane();
 
         Text title = new Text("Login");
@@ -95,7 +109,7 @@ public class App extends Application {
 
         final Text loginStatus = new Text();
         gridPane.add(loginStatus, 1, 4);
-
+        gridPane.setAlignment(Pos.CENTER);
         // Add a handler for the login button (API)
         loginButton.setOnAction(event -> {
             String username = usernameTextField.getText();
@@ -107,9 +121,51 @@ public class App extends Application {
         loginGroup.getChildren().add(borderPane);
 
         ///
+        /// Main page (main scene)
+        ///
 
+        Label topLabel = new Label("Welcome to the Tournament App");
+        topLabel.setStyle("-fx-font-size: 24; -fx-font-weight: bold;");
+
+        Button exploreTournamentsButton = new Button("Explore Tournaments");
+        exploreTournamentsButton.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white; -fx-font-size: 16;");
+        exploreTournamentsButton.setPrefSize(200, 60);
+
+        Button addNewTournamentButton = new Button("Add New Tournament");
+        addNewTournamentButton.setStyle("-fx-background-color: #2196F3; -fx-text-fill: white; -fx-font-size: 16;");
+        addNewTournamentButton.setPrefSize(200, 60);
+
+        Button addScoreButton = new Button("Add Score");
+        addScoreButton.setStyle("-fx-background-color: #F44336; -fx-text-fill: white; -fx-font-size: 16;");
+        addScoreButton.setPrefSize(200, 60);
+
+        Image homeIcon = new Image("home_icon.png", 50, 50, true, true);
+        ImageView homeImageView = new ImageView(homeIcon);
+
+        Image tournamentIcon = new Image("tournament_icon.png", 50, 50, true, true);
+        ImageView tournamentImageView = new ImageView(tournamentIcon);
+
+        Image personalIcon = new Image("profile_icon.png", 50, 50, true, true);
+        ImageView personalImageView = new ImageView(personalIcon);
+
+        HBox bottomBar = new HBox(30);
+        bottomBar.setAlignment(Pos.CENTER);
+        bottomBar.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+        bottomBar.setPadding(new Insets(10));
+        bottomBar.getChildren().addAll(homeImageView, tournamentImageView, personalImageView);
+
+        // root pane and add all elements
+        VBox root = new VBox(50);
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(20));
+        root.getChildren().addAll(topLabel, exploreTournamentsButton, addNewTournamentButton, addScoreButton, bottomBar);
+
+        mainGroup.getChildren().add(root);
         
-        
+        ///
+        /// Add tournament page (add tournament scene)
+        ///
+
         primaryStage.setScene(loginScene);
         primaryStage.show();
     }

6baa243d58697b47e1040f8b0b84d9d6b1c88ae9
Ahmed Al Mughirah <s202018480@kfupm.edu.sa>
2023-05-13 20:08:58 +0300

    Rank

--- Team.java
+++ Team.java
@@ -2,7 +2,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Comparator;
 
-public class Team implements Serializable,Comparable<Team>{
+public class Team implements Serializable, Comparable<Team> {
     private ArrayList<Student> students;
     private String name;
     private Tournament enrolledTournament;
@@ -36,47 +36,59 @@ public class Team implements Serializable,Comparable<Team>{
     public Tournament getTournament() {
         return this.enrolledTournament;
     }
+
     public Tournament setTournament(Tournament t) {
         return this.enrolledTournament = t;
     }
 
-
-
-    public void updatePoints(int point){
+    public void updatePoints(int point) {
         points += point;
     }
 
-    public void updateScore(int scoreFor, int scoreAgainst){
+    public void updateScore(int scoreFor, int scoreAgainst) {
         this.scoreFor += scoreFor;
         this.scoreAgainst += scoreAgainst;
     }
-    public String toString(){
+
+    public String toString() {
         return name;
     }
 
-  
-    public int getPoints(){
+    public int getPoints() {
         return this.points;
     }
 
-    
-    
-
     @Override
     public int compareTo(Team o) {
-        return Integer.compare(this.points, o.points);
+        if (this.points > o.points) {
+            return 1;
+        } else if (this.points < o.points) {
+            return -1;
+        } else {
+            if (this.scoreFor > o.scoreFor) {
+                return 1;
+            } else if (this.scoreFor < o.scoreFor) {
+                return -1;
+            } else {
+                if (this.scoreAgainst < o.scoreAgainst) {
+                    return 1;
+                } else if (this.scoreAgainst > o.scoreAgainst) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        }
     }
-
-    
 
     // @Override
     // public int compare(Team o1, Team o2) {
-    //     if(o1.points > o2.points){
-    //         return 1;
-    //     }else if(o1.points < o2.points){
-    //         return -1;
-    //     }else{
-    //         return 0;
-    //     }
+    // if(o1.points > o2.points){
+    // return 1;
+    // }else if(o1.points < o2.points){
+    // return -1;
+    // }else{
+    // return 0;
+    // }
     // }
 }
